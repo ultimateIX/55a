48a2ec5ff791c11590cd593baa4b2c706ade9864
unknown <rv17012@ues.edu.sv>
2020-11-21 15:48:43 -0600

    css y biblioteca

--- biblioteca.sql
+++ biblioteca.sql
@@ -14,10 +14,6 @@
 # ------------------------------------------------------
 # Server version 5.5.5-10.1.13-MariaDB
 
-DROP DATABASE IF EXISTS `biblioteca`;
-CREATE DATABASE `biblioteca` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `biblioteca`;
-
 #
 # Source for table editorial
 #
@@ -27,7 +23,7 @@ CREATE TABLE `editorial` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(40) CHARACTER SET latin1 NOT NULL DEFAULT 'Panini',
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 #
 # Dumping data for table editorial
@@ -68,6 +64,31 @@ INSERT INTO `libro` VALUES (2,'9283019230','Especies útiles de la flora salvador
 INSERT INTO `libro` VALUES (3,'2973811023','Edipo rey','Sófocles','El volcán','0430-09-30');
 INSERT INTO `libro` VALUES (4,'0000000000','Psicología del éxito','Mario Luna','Caja negra','2015-06-01');
 /*!40000 ALTER TABLE `libro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+#
+# Source for table usuarios
+#
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(15) NOT NULL DEFAULT 'Anonimo',
+  `clave` varchar(255) NOT NULL DEFAULT '1',
+  `nombre` varchar(100) CHARACTER SET latin1 COLLATE latin1_spanish_ci NOT NULL DEFAULT 'Anonimo Anonimo',
+  `nivel` int(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+#
+# Dumping data for table usuarios
+#
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Digitador','1','Pedro Perez',1);
+INSERT INTO `usuarios` VALUES (2,'Mantenimiento','2','Juan Ramirez',2);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
